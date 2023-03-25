c169b4488face24359bcbe1ccae99a7cbeea159d
Oluwaseun Akinola <seunakinola082@gmail.com>
2023-03-26 00:58:07 +0100

    Pulling username from state  into checkout page and showing the user login email.

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,13 +5,15 @@ import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
   return (
     <div className='checkout'>
         <div className='checkout__left'>
             <img className='checkout__ad'
-                  src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61jovjd+f9L._SX3000_.jpg' alt='' />
+                  src='#https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61jovjd+f9L._SX3000_.jpg' alt='' />
+
             <div>
+                <h3>Hello, {user?.email}</h3>
                 <h2 className='checkout__title'>Your shopping Basket</h2>
                 {basket.map(item =>(
                        <CheckoutProduct
--- src/Header.js
+++ src/Header.js
@@ -31,7 +31,7 @@ function Header() {
             <Link to={ !user && "/login" }> 
             <div onClick={ handleAuthentication } className='header__option'>
                 <span className='header__optionLineOne'>
-                    Hello Guest!
+                    Hello, {user?.email}
                 </span>
                 <span className='header__optionLineTwo'>{user ?
                  'Sign out' : 'Sign In' } </span>
